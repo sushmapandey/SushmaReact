42bad88d088b0943a429762a61ffbf542d5a164d
Nikhil <nikhil.pc@terralogic.com>
2023-06-01 18:27:25 +0530

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let valuedata = "https://engineering-task.elancoapps.com/api/resources";
+
+  const [data, setData] = useState([]);
+  const [valData, setValData] = useState(data);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    try {
+      const response = await fetch(valuedata);
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
+  useEffect(() => {
+    setValData(data);
+  }, []);
+
+  let sortdata = () => {
+    let val = data.sort();
+    console.log(val, "value of data");
+    setValData(val);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {valData.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+
+      <button onClick={() => sortdata()}>Sort</button>
     </div>
   );
 }
